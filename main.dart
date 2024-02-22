d5b28bd55d8a66d5dab49f341f0110cd8d2f517b
veranyagaka <nyagakaver@gmail.com>
2024-02-22 21:40:05 +0300

    Initial program

---
+++ main.dart
@@ -0,0 +1,12 @@
+void main() {
+  print("Hello Vera!");
+  //variables
+  String name = "Vera Nyagaka";
+  int age = 19;
+  bool issingle = true;
+  print(name);
+  print(age);
+  print(issingle);
+  print(
+      "My name is $name. I am $age years old and it is $issingle that I am single!!!");
+}
