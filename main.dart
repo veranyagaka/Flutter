9855eb550804a83f938248ce8ca7237be03e35aa
veranyagaka <nyagakaver@gmail.com>
2024-02-29 19:44:30 +0300

    user input and functions

--- main.dart
+++ main.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 void main() {
-  print("Hello Vera!");
+  String? firstname = stdin.readLineSync()!;
+  print("Hello $firstname!");
   //variables
   String name = "Vera Nyagaka";
   int age = 19;
@@ -9,4 +12,30 @@ void main() {
   print(issingle);
   print(
       "My name is $name. I am $age years old and it is $issingle that I am single!!!");
+  List<String> names = ["Vera", "Nyagaka"];
+  print(names);
+  Map<String, int> wealthy = {"Vera": 19};
+  print(wealthy.keys);
+  print(wealthy.values);
+  print(wealthy);
+
+  //runes
+  String rune = "Rune in Dart \u{1F600}";
+//maps
+//list
+  print(rune);
+
+  printName();
+
+  addNum(2, 0.10);
+}
+
+//functions
+void printName() {
+  print('Vera is awesome');
+}
+
+void addNum(num1, num2) {
+  num sum = num1 + num2;
+  print(sum);
 }
